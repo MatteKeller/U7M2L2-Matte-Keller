e31e6a8b8065e3f65340e257838cfa415dee4a85
MatteKeller <112646286+MatteKeller@users.noreply.github.com>
2023-06-08 19:34:40 -0400

    Add files via upload

---
+++ My_code.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	
+ $(document).ready(function() {
+      $("h1").click(function() {
+        $("h1").toggle("slow");
+		
+		$(document).ready(function() {
+      $("h2").click(function() {
+        $("h2").slideDown("fast");
+		
+		$(document).ready(function() {
+      $("h3").click(function() {
+        $("h3").slideUp("fast");
+    });
+});
